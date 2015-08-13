dc177712e93455b3576a1a7e493ddc09490c6242
Taylor Van Orden <tayvano@gmail.com>
2015-08-12 21:13:30 -0700

    Initial Bootstrap & Gulp Files

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+// less
+var gulp = require('gulp');
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
+var notify = require('gulp-notify');
+var rename = require("gulp-rename");
+var uncss = require('gulp-uncss');
+
+// js
+var gulpConcat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+// less vars
+var lessWatchFolder = './css/less/**/*.less';
+var lessFile = './css/less/etherwallet-master.less';
+var lessOutputFolder = './css';
+var lessOutputFile = 'etherwallet-master.css';
+var lessOutputFileMin = 'etherwallet-master.min.css';
+
+//js vars
+var jsWatchFolder = './js/**/*.js';
+var jsFiles = "./js/**/*.js";
+var jsOutputFolder = './js';
+var jsOutputFile = 'etherwallet-master.js';
+
+gulp.task('less', function (cb) {
+  return gulp
+    .src(lessFile)
+      .pipe(less({ compress: false })).on('error', notify.onError(function (error) {
+        return "ERROR! Problem file : " + error.message;
+      }))
+      .pipe(autoprefixer('last 2 version', 'ie 9', 'ios 6', 'android 4'))
+      .pipe(rename(lessOutputFile))
+      .pipe(gulp.dest(lessOutputFolder))
+
+      .pipe(minifyCSS({keepBreaks: false}))
+      .pipe(rename(lessOutputFileMin))
+      .pipe(gulp.dest(lessOutputFolder))
+      .pipe(notify('Less Compiled and Minified'));
+});
+
+gulp.task('less-uncss', ['less'], function () {
+  return gulp
+    .src('./css/' + lessOutputFile)
+      .pipe(uncss({
+        html: [
+          './index.html'
+        ]
+      }))
+      .pipe(minifyCSS({keepBreaks: false}))
+      .pipe(rename(lessOutputFileMin))
+      .pipe(gulp.dest(lessOutputFolder))
+    .pipe(notify('Less UnCSS and Minified'));
+});
+
+gulp.task('minJS', function () {
+  return gulp
+    .src(jsFiles)
+      .pipe(gulpConcat(jsOutputFile))
+      .pipe(uglify())
+      .pipe(gulp.dest(jsOutputFolder))
+    .pipe(notify('JS Concat and Uglified'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(lessWatchFolder, ['less', 'watch']);
+    gulp.watch(jsWatchFolder, ['minJS', 'watch']);
+});
+
+
+gulp.task('default', ['less' , 'less-uncss', 'minJS' , 'watch']);
