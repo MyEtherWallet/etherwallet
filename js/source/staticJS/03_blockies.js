953047d695efc6d5f41b19c248a23ec3db7de9ae
Taylor Van Orden <tayvano@gmail.com>
2016-03-09 19:37:30 -0800

    blockies update (no change as far as I can tell)

--- js/source/staticJS/03_blockies.js
+++ js/source/staticJS/03_blockies.js
@@ -1,2 +1,110 @@
-(function(){function h(){var b=a[0]^a[0]<<11;a[0]=a[1];a[1]=a[2];a[2]=a[3];a[3]=a[3]^a[3]>>19^b^b>>8;return(a[3]>>>0)/2147483648}function q(){var b=Math.floor(360*h()),a=60*h()+40+"%",k=25*(h()+h()+h()+h())+"%";return"hsl("+b+","+a+","+k+")"}var a=Array(4);window.blockies={create:function(b){b=b||{};for(var d=b.size||10,k=b.scale||5,n=b.seed||Math.random().toString(36).substr(2),c=0;c<a.length;c++)a[c]=0;for(c=0;c<n.length;c++)a[c%4]=(a[c%4]<<5)-a[c%4]+n.charCodeAt(c);var n=b.color||q(),c=q(),f=b.bgcolor||
-q(),m=Math.ceil(d/2),l=d-m;b=[];for(var p=0;p<d;p++){for(var g=[],e=0;e<m;e++)g[e]=Math.floor(2.3*h());e=g.slice(0,l);e.reverse();g=g.concat(e);for(e=0;e<g.length;e++)b.push(g[e])}d=document.createElement("canvas");m=Math.sqrt(b.length);d.width=d.height=m*k;l=d.getContext("2d");l.fillStyle=f;l.fillRect(0,0,d.width,d.height);l.fillStyle=n;for(f=0;f<b.length;f++)p=Math.floor(f/m),g=f%m,l.fillStyle=1==b[f]?n:c,b[f]&&l.fillRect(g*k,p*k,k,k);return d}}})();
+// https://github.com/alexvandesande/blockies/blob/master/blockies.js
+// updated tayvano 3.9.16
+(function() {
+  // The random number is a js implementation of the Xorshift PRNG
+  var randseed = new Array(4); // Xorshift: [x, y, z, w] 32 bit values
+
+  function seedrand(seed) {
+    for (var i = 0; i < randseed.length; i++) {
+      randseed[i] = 0;
+    }
+    for (var i = 0; i < seed.length; i++) {
+      randseed[i%4] = ((randseed[i%4] << 5) - randseed[i%4]) + seed.charCodeAt(i);
+    }
+  }
+
+  function rand() {
+    // based on Java's String.hashCode(), expanded to 4 32bit values
+    var t = randseed[0] ^ (randseed[0] << 11);
+
+    randseed[0] = randseed[1];
+    randseed[1] = randseed[2];
+    randseed[2] = randseed[3];
+    randseed[3] = (randseed[3] ^ (randseed[3] >> 19) ^ t ^ (t >> 8));
+
+    return (randseed[3]>>>0) / ((1 << 31)>>>0);
+  }
+
+  function createColor() {
+    //saturation is the whole color spectrum
+    var h = Math.floor(rand() * 360);
+    //saturation goes from 40 to 100, it avoids greyish colors
+    var s = ((rand() * 60) + 40) + '%';
+    //lightness can be anything from 0 to 100, but probabilities are a bell curve around 50%
+    var l = ((rand()+rand()+rand()+rand()) * 25) + '%';
+
+    var color = 'hsl(' + h + ',' + s + ',' + l + ')';
+    return color;
+  }
+
+  function createImageData(size) {
+    var width = size; // Only support square icons for now
+    var height = size;
+
+    var dataWidth = Math.ceil(width / 2);
+    var mirrorWidth = width - dataWidth;
+
+    var data = [];
+    for(var y = 0; y < height; y++) {
+      var row = [];
+      for(var x = 0; x < dataWidth; x++) {
+        // this makes foreground and background color to have a 43% (1/2.3) probability
+        // spot color has 13% chance
+        row[x] = Math.floor(rand()*2.3);
+      }
+      var r = row.slice(0, mirrorWidth);
+      r.reverse();
+      row = row.concat(r);
+
+      for(var i = 0; i < row.length; i++) {
+        data.push(row[i]);
+      }
+    }
+
+    return data;
+  }
+
+  function createCanvas(imageData, color, scale, bgcolor, spotcolor) {
+    var c = document.createElement('canvas');
+    var width = Math.sqrt(imageData.length);
+    c.width = c.height = width * scale;
+
+    var cc = c.getContext('2d');
+    cc.fillStyle = bgcolor;
+    cc.fillRect(0, 0, c.width, c.height);
+    cc.fillStyle = color;
+
+    for(var i = 0; i < imageData.length; i++) {
+      var row = Math.floor(i / width);
+      var col = i % width;
+      // if data is 2, choose spot color, if 1 choose foreground
+      cc.fillStyle = (imageData[i] == 1) ? color : spotcolor;
+
+      // if data is 0, leave the background
+      if(imageData[i]) {
+        cc.fillRect(col * scale, row * scale, scale, scale);
+      }
+    }
+
+    return c;
+  }
+
+  function createIcon(opts) {
+    opts = opts || {};
+    var size = opts.size || 8;
+    var scale = opts.scale || 4;
+    var seed = opts.seed || Math.floor((Math.random()*Math.pow(10,16))).toString(16);
+
+    seedrand(seed);
+
+    var color = opts.color || createColor();
+    var bgcolor = opts.bgcolor || createColor();
+    var spotcolor = opts.spotcolor || createColor();
+    var imageData = createImageData(size);
+    var canvas = createCanvas(imageData, color, scale, bgcolor, spotcolor);
+
+    return canvas;
+  }
+
+  window.blockies = {create: createIcon};
+})();
