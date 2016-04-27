e3f365aa6a3f9670a17ca6655b0453b73f5a357e
Taylor Van Orden <tayvano@gmail.com>
2016-04-27 16:03:20 -0700

    turned dao and contracts pages back on to make tweaks. HIDE CONTRACTS BEFORE PUSHING DAO LIVE!

--- js/etherwallet-master.js
+++ js/etherwallet-master.js
@@ -1240,13 +1240,23 @@ var globalService = function($http, $httpParamSerializerJQLike) {
       name: "Offline Transaction",
             url:"offline-transaction"
     },
-    digix: {
+    thedao: {
       id: 5,
+      name: "\"The DAO\"",
+            url: "the-dao"
+    },
+    digix: {
+      id: 6,
       name: "Digix",
             url: "digix"
     },
+    contracts: {
+      id: 7,
+      name: "Contracts",
+            url: "contracts"
+    },
     help: {
-      id: 6,
+      id: 8,
       name: "Help",
             url: "help"
     }
