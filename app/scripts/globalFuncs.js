2eca59baf06f9eff055dde1614fdb062dd2fcc18
kvhnuke <kvhnuke@aol.com>
2016-04-28 18:57:44 -0700

    optimized post equests by adding them to a queue

--- app/scripts/globalFuncs.js
+++ app/scripts/globalFuncs.js
@@ -47,6 +47,7 @@ globalFuncs.successMsgs = [ "Valid address",
 globalFuncs.scrypt = {
 	n: 1024
 };
+globalFuncs.postDelay = 300;
 globalFuncs.kdf = "scrypt";
 globalFuncs.defaultTxGasLimit = 21000;
 globalFuncs.digixClaimTxGasLimit = 150000;
