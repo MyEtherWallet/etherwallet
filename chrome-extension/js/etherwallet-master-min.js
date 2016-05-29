3cb195995819674935981bbccfe1221366fb3e94
Taylor Van Orden <tayvano@gmail.com>
2016-05-29 13:26:46 -0700

    update dao proposal controller

--- chrome-extension/js/etherwallet-master-min.js
+++ chrome-extension/js/etherwallet-master-min.js
@@ -1190,12 +1190,12 @@ module.exports = theDaoCtrl;
 var theDaoProposalCtrl = function($scope, $sce, walletService) {
 	$scope.curTab = "send";
 	new Modal(document.getElementById('sendTransaction'));
-    $scope.voteModal = new Modal(document.getElementById('voteProposal'));
+	$scope.voteModal = new Modal(document.getElementById('voteProposal'));
 	walletService.wallet = null;
 	walletService.password = '';
 	$scope.showAdvance = false;
 	$scope.showRaw = false;
-	$scope.slockitContract = "0xd838f9c9792bf8398e1f5fbfbd3b43c5a86445aa"; // 0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413
+	$scope.slockitContract = "0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413"; //0xd838f9c9792bf8398e1f5fbfbd3b43c5a86445aa
 	$scope.slockitBalance = "0x70a08231";
 	$scope.slockitSupply = "0x18160ddd";
 	$scope.slockitTransfer = "0xa9059cbb";
@@ -1203,7 +1203,7 @@ var theDaoProposalCtrl = function($scope, $sce, walletService) {
 	$scope.slockitminQuorumDivisor = "0x674ed066";
 	$scope.slockitABalance = "0x39d1f908";
 	$scope.slockitRToken = "0xcdef91d0";
-    $scope.slockitVote = "0xc9d27afe";
+	$scope.slockitVote = "0xc9d27afe";
 	$scope.tx = {
 		gasLimit: 150000,
 		data: '',
@@ -1309,8 +1309,8 @@ var theDaoProposalCtrl = function($scope, $sce, walletService) {
 			$scope.validateTxStatus = $sce.trustAsHtml(globalFuncs.getDangerText(e));
 		}
 	}
-    $scope.generateVoteTx = function(isYes) {
-        $scope.voteTxStatus = true;
+	$scope.generateVoteTx = function(isYes) {
+		$scope.voteTxStatus = true;
 		try {
 			$scope.tx.to = $scope.slockitContract;
 			var id = ethFuncs.padLeft(new BigNumber($scope.proposalId).toString(16), 64);
@@ -1318,19 +1318,19 @@ var theDaoProposalCtrl = function($scope, $sce, walletService) {
 			$scope.tx.data = $scope.slockitVote + id + vote;
 			$scope.tx.value = 0;
 			$scope.voteTxStatus = $sce.trustAsHtml(globalFuncs.getDangerText(''));
-            $scope.autoSend = true;
+			$scope.autoSend = true;
 			$scope.generateTx();
 		} catch (e) {
 			$scope.showRaw = false;
 			$scope.voteTxStatus = $sce.trustAsHtml(globalFuncs.getDangerText(e));
 		}
-        $scope.voteModal.close();
+		$scope.voteModal.close();
 	}
 	$scope.setProposal = function() {
 		try {
 			$scope.loadProposalStatus = "";
 			if (!globalFuncs.isNumeric($scope.proposalId) || parseFloat($scope.proposalId) < 0) throw globalFuncs.errorMsgs[15];
-			var callProposal = ethFuncs.getDataObj($scope.slockitContract, $scope.slockitProposal, [$scope.proposalId]);
+			var callProposal = ethFuncs.getDataObj($scope.slockitContract, $scope.slockitProposal, [new BigNumber($scope.proposalId).toString(16)]);
 			ajaxReq.getEthCall(callProposal, function(data) {
 				try {
 					if (data.error) {
@@ -1338,40 +1338,39 @@ var theDaoProposalCtrl = function($scope, $sce, walletService) {
 					} else {
 						var proposal = ethFuncs.contractOutToArray(data.data);
 						$scope.objProposal = {
-							id: $scope.proposalId,
-							recipient: '0x' + proposal[0],
-							amount: etherUnits.toEther('0x' + proposal[1], 'wei'),
-							content: proposal.slice(13).join(),
-							description: globalFuncs.hexToAscii(proposal.slice(13).join('')).replace(/<br>/g, '\n').replace(/\\n/g, '\n'),
+							id: 						$scope.proposalId,
+							recipient: 			'0x' + proposal[0],
+							amount: 				etherUnits.toEther('0x' + proposal[1], 'wei'),
+							content: 				proposal[12]=="0" ? "" : proposal.slice(13).join(),
+							description:  	proposal[12]=="0" ? "" : globalFuncs.hexToAscii(proposal.slice(13).join('')).replace(/<br>/g, '\n').replace(/\\n/g, '\n'),
 							votingDeadline: new Date(new BigNumber("0x" + proposal[3]).toNumber() * 1000),
-                            today: new Date(),
-							open: proposal[4] == '1' ? "Yes" : "No",
+							today: 					new Date(),
+							open: 					proposal[4] == '1' ? "Yes" : "No",
 							proposalPassed: proposal[5] == '1' ? "Yes" : "No",
-							proposalHash: '0x'+proposal[6],
-							proposalDeposit: etherUnits.toEther('0x' + proposal[7], 'wei'),
-							split: proposal[8] == '1' ? "Yes" : "No",
-							yea: etherUnits.toEther('0x' + proposal[9], 'wei'),
-							nay: etherUnits.toEther('0x' + proposal[10], 'wei'),
-							creator: "0x" + proposal[11],
-							enabled: true,
+							proposalHash: 	'0x' + proposal[6],
+							proposalDeposit:etherUnits.toEther('0x' + proposal[7], 'wei'),
+							split:  				proposal[8] == '1' ? "Yes" : "No",
+							yea: 						etherUnits.toEther('0x' + proposal[9], 'wei'),
+							nay: 						etherUnits.toEther('0x' + proposal[10], 'wei'),
+							creator: 				"0x" + proposal[11],
+							enabled: 				true,
 							minQuroum: function() {
 								var totalInWei = etherUnits.toWei($scope.token.totRaised, "ether");
 								return etherUnits.toEther(totalInWei / $scope.minQuorumDivisor + (etherUnits.toWei(this.amount, "ether") * totalInWei) / (3 * ($scope.actualBalance + $scope.rewardToken)), "wei");
 							},
-							data: proposal
+							data: 					proposal
 						};
 						var yeaBN = new BigNumber($scope.objProposal.yea);
 						var nayBN = new BigNumber($scope.objProposal.nay);
 						$scope.objProposal.totalVotes = yeaBN.plus(nayBN)
-						$scope.objProposal.yeaPer = yeaBN.plus(nayBN).toNumber()=='0' ? 0 : yeaBN.div( $scope.objProposal.totalVotes ).times(100).toNumber();
-						$scope.objProposal.nayPer = yeaBN.plus(nayBN).toNumber()=='0' ? 0 : nayBN.div( $scope.objProposal.totalVotes ).times(100).toNumber();
-            $scope.showProposal = true;
-						$scope.objProposal.quorumCurrent = ( $scope.objProposal.totalVotes *100 ) / $scope.token.totRaised;
-						$scope.objProposal.quorumPer = ( $scope.objProposal.minQuroum()*100 ) / $scope.token.totRaised;
-
+						$scope.objProposal.yeaPer = yeaBN.plus(nayBN).toNumber() == '0' ? 0 : yeaBN.div($scope.objProposal.totalVotes).times(100).toNumber();
+						$scope.objProposal.nayPer = yeaBN.plus(nayBN).toNumber() == '0' ? 0 : nayBN.div($scope.objProposal.totalVotes).times(100).toNumber();
+						$scope.showProposal = true;
+						$scope.objProposal.quorumCurrent = ($scope.objProposal.totalVotes * 100) / $scope.token.totRaised;
+						$scope.objProposal.quorumPer = ($scope.objProposal.minQuroum() * 100) / $scope.token.totRaised;
 					}
 				} catch (e) {
-					$scope.loadProposalStatus = $sce.trustAsHtml(globalFuncs.errorMsgs[15]);
+					$scope.loadProposalStatus = $sce.trustAsHtml(globalFuncs.getDangerText(globalFuncs.errorMsgs[15]+e));
 					$scope.showProposal = false;
 				}
 			});
@@ -1395,6 +1394,8 @@ var theDaoProposalCtrl = function($scope, $sce, walletService) {
 	}
 };
 module.exports = theDaoProposalCtrl;
+
+
 
 },{}],15:[function(require,module,exports){
 'use strict';
