39b2074ea719e474b5377594bcdc161898422eec
mikhail-vladimirov <vladimirow@mail.ru>
2016-08-23 13:23:45 +0400

    Update translate.js

--- app/scripts/translations/translate.js
+++ app/scripts/translations/translate.js
@@ -5,6 +5,7 @@ var es = require('./es');
 var fr = require('./fr');
 var ko = require('./ko');
 var no = require('./no');
+var ru = require('./ru');
 var sl = require('./sl');
 var it = require('./it');
 var vi = require('./vi');
@@ -12,15 +13,16 @@ var test = require('./test');
 
 var translate = function($translateProvider) {
 	$translateProvider.translations(en.code, translate.marked(en.data));
-  $translateProvider.translations(de.code, translate.marked(de.data));
-  $translateProvider.translations(es.code, translate.marked(es.data));
-  $translateProvider.translations(fr.code, translate.marked(fr.data));
-  $translateProvider.translations(ko.code, translate.marked(ko.data));
-  $translateProvider.translations(no.code, translate.marked(no.data));
-  $translateProvider.translations(sl.code, translate.marked(sl.data));
-  $translateProvider.translations(it.code, translate.marked(it.data));
-  $translateProvider.translations(vi.code, translate.marked(vi.data));
-  $translateProvider.translations(test.code, translate.marked(test.data));
+	$translateProvider.translations(de.code, translate.marked(de.data));
+	$translateProvider.translations(es.code, translate.marked(es.data));
+	$translateProvider.translations(fr.code, translate.marked(fr.data));
+	$translateProvider.translations(ko.code, translate.marked(ko.data));
+	$translateProvider.translations(no.code, translate.marked(no.data));
+	$translateProvider.translations(ru.code, translate.marked(ru.data));
+	$translateProvider.translations(sl.code, translate.marked(sl.data));
+	$translateProvider.translations(it.code, translate.marked(it.data));
+	$translateProvider.translations(vi.code, translate.marked(vi.data));
+	$translateProvider.translations(test.code, translate.marked(test.data));
 	$translateProvider.preferredLanguage('en');
 	$translateProvider.useSanitizeValueStrategy(null);
 }
