ac66538e4e9d5fb49c5de12da1f19bcb0b15d547
kvhnuke <kvhnuke@aol.com>
2016-08-25 22:25:43 -0700

    load language from local storage

--- app/scripts/controllers/tabsCtrl.js
+++ app/scripts/controllers/tabsCtrl.js
@@ -3,14 +3,14 @@ var tabsCtrl = function($scope, globalService, $translate) {
 	$scope.tabNames = globalService.tabs;
 	$scope.curLang = "English";
 	var hval = window.location.hash;
-    $scope.setArrowVisibility = function (){
-        setTimeout(function(){
-            $scope.showLeftArrow = false;
-            $scope.showRightArrow = !(document.querySelectorAll(".nav-inner")[0].clientWidth<=document.querySelectorAll(".nav-scroll")[0].clientWidth);
-            $scope.$apply();
-        },200);
-    }
-    $scope.setArrowVisibility();
+	$scope.setArrowVisibility = function() {
+		setTimeout(function() {
+			$scope.showLeftArrow = false;
+			$scope.showRightArrow = !(document.querySelectorAll(".nav-inner")[0].clientWidth <= document.querySelectorAll(".nav-scroll")[0].clientWidth);
+			$scope.$apply();
+		}, 200);
+	}
+	$scope.setArrowVisibility();
 	$scope.setTab = function(hval) {
 		if (hval != "") {
 			hval = hval.replace("#", '');
@@ -25,7 +25,7 @@ var tabsCtrl = function($scope, globalService, $translate) {
 			$scope.activeTab = globalService.currentTab;
 		}
 	}
-    $scope.setTab(hval);
+	$scope.setTab(hval);
 	$scope.tabClick = function(id) {
 		$scope.activeTab = globalService.currentTab = id;
 		for (var key in $scope.tabNames) {
@@ -35,35 +35,48 @@ var tabsCtrl = function($scope, globalService, $translate) {
 	$scope.changeLanguage = function(key, value) {
 		$translate.use(key);
 		$scope.curLang = value;
-        $scope.setArrowVisibility();
+		$scope.setArrowVisibility();
+		localStorage.setItem("language", JSON.stringify({
+			key: key,
+			value: value
+		}));
 	};
-    $scope.setHash = function(hash){
-        location.hash = hash;
-        $scope.setTab(hash);
-        $scope.$apply();
-    }
-    $scope.scrollHoverIn = function(isLeft, val) {
-        clearInterval($scope.sHoverTimer);
-        $scope.sHoverTimer = setInterval(function(){
-            if(isLeft) $scope.scrollLeft(val);
-            else $scope.scrollRight(val);
-        },20);
-    }
-    $scope.scrollHoverOut = function() {
-        clearInterval($scope.sHoverTimer);
-    }
-    $scope.scrollLeft = function(val){
-        var ele = document.querySelectorAll(".nav-scroll")[0];
-        ele.scrollLeft -= val;
-        $scope.showLeftArrow = ele.scrollLeft > 0;
-        $scope.showRightArrow = document.querySelectorAll(".nav-inner")[0].clientWidth > (ele.clientWidth+ele.scrollLeft);
-    }
-    $scope.scrollRight = function(val){
-        var ele = document.querySelectorAll(".nav-scroll")[0];
-        ele.scrollLeft += val;
-        $scope.showLeftArrow = ele.scrollLeft > 0;
-        $scope.showRightArrow = document.querySelectorAll(".nav-inner")[0].clientWidth > (ele.clientWidth+ele.scrollLeft);
-    }
-    globalFuncs.changeHash = $scope.setHash;
+	$scope.setLanguageFromStorage = function() {
+		var lang = localStorage.getItem('language');
+		if (lang == null) return;
+		lang = JSON.parse(lang);
+		var key = globalFuncs.stripTags(lang.key);
+		var value = globalFuncs.stripTags(lang.value);
+		$scope.changeLanguage(key, value);
+	}
+	$scope.setLanguageFromStorage();
+	$scope.setHash = function(hash) {
+		location.hash = hash;
+		$scope.setTab(hash);
+		$scope.$apply();
+	}
+	$scope.scrollHoverIn = function(isLeft, val) {
+		clearInterval($scope.sHoverTimer);
+		$scope.sHoverTimer = setInterval(function() {
+			if (isLeft) $scope.scrollLeft(val);
+			else $scope.scrollRight(val);
+		}, 20);
+	}
+	$scope.scrollHoverOut = function() {
+		clearInterval($scope.sHoverTimer);
+	}
+	$scope.scrollLeft = function(val) {
+		var ele = document.querySelectorAll(".nav-scroll")[0];
+		ele.scrollLeft -= val;
+		$scope.showLeftArrow = ele.scrollLeft > 0;
+		$scope.showRightArrow = document.querySelectorAll(".nav-inner")[0].clientWidth > (ele.clientWidth + ele.scrollLeft);
+	}
+	$scope.scrollRight = function(val) {
+		var ele = document.querySelectorAll(".nav-scroll")[0];
+		ele.scrollLeft += val;
+		$scope.showLeftArrow = ele.scrollLeft > 0;
+		$scope.showRightArrow = document.querySelectorAll(".nav-inner")[0].clientWidth > (ele.clientWidth + ele.scrollLeft);
+	}
+	globalFuncs.changeHash = $scope.setHash;
 };
 module.exports = tabsCtrl;
