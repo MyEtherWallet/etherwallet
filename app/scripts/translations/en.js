50f3ffc2179e6ff5ef021253f255d47361e9b157
Kevin <kevinmonahan@users.noreply.github.com>
2016-08-26 20:25:10 -0700

    number consistency

--- app/scripts/translations/en.js
+++ app/scripts/translations/en.js
@@ -193,7 +193,7 @@ en.data = {
   /* DAO */
   DAO_Desc:               'Use this tab to Withdraw your DAO Tokens for ETH **& ETC**. If you wish to send DAO, please use the Send Tokens Tab.',
   DAO_Inst:               'Yes. Just push the big red button. It\'s that easy.',
-  DAO_Warning:            'If you are getting an "Insufficient balance for gas" error, you must have a small amount of ETH in your account in order to cover the cost of gas. Add .01 ETH to this account and try again. ',
+  DAO_Warning:            'If you are getting an "Insufficient balance for gas" error, you must have a small amount of ETH in your account in order to cover the cost of gas. Add 0.001 ETH to this account and try again. ',
   DAOModal_Title:         'Just making sure...',
 
   /* Digix */
@@ -244,7 +244,7 @@ en.data = {
   ERROR_15:             'Wallet not found. ',
   ERROR_16:             'It doesnt look like a proposal with this ID exists yet or there is an error reading this proposal. ',
   ERROR_17:             'A wallet with this address already exists in storage. Please check your wallets page. ',
-  ERROR_18:             'You need to have at least .001 ether in your account to cover the cost of gas. Please add some ether and try again. ',
+  ERROR_18:             'You need to have at least 0.001 ETH in your account to cover the cost of gas. Please add some ETH and try again. ',
   ERROR_19:             'All gas would be used on this transaction. This means you have already voted on this proposal or the debate period has ended.',
   ERROR_20:             'Invalid symbol',
   SUCCESS_1:            'Valid address',
