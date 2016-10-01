6e37c4aa8f2954dc8dfdef1ac162bb5477088d49
Taylor Van Orden <tayvano@gmail.com>
2016-09-30 17:44:28 -0700

    added token stuff to offline tab

--- app/scripts/translations/en.js
+++ app/scripts/translations/en.js
@@ -8,6 +8,7 @@ en.data = {
   /* Tay TODO: add to all language files, reach out to people to translate, update any other pages at that time too */
   x_Mnemonic:           'Mnemonic Phrase',
   ADD_Radio_5:          'Paste/Type Your Mnemonic',
+  SEND_custom:           'Custom Token',
 
   /* Navigation*/
   NAV_YourWallets:      'Your Wallets',
@@ -115,7 +116,6 @@ en.data = {
   SEND_addr:             'To Address: ',
   SEND_amount:           'Amount to Send: ',
   SEND_amount_short:     'Amount',
-  SEND_custom:           'Custom',
   SEND_gas:              'Gas',
   SEND_TransferTotal:   'Send Entire Balance',
   SEND_generate:         'Generate Transaction',
