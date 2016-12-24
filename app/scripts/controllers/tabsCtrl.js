c3eb3582a590466a834f041d0fceca768d28cd5e
Taylor Van Orden <tayvano@gmail.com>
2016-12-24 13:26:40 -0800

    new node setup

--- app/scripts/controllers/tabsCtrl.js
+++ app/scripts/controllers/tabsCtrl.js
@@ -1,148 +1,215 @@
 'use strict';
 var tabsCtrl = function($scope, globalService, $translate) {
-	$scope.tabNames = globalService.tabs;
-	$scope.curLang = "English";
-	$scope.curNode = "Select Node"; //todo = fetch from localstorage
-	var hval = window.location.hash;
+  $scope.tabNames = globalService.tabs;
+  $scope.curLang = 'English';
 
-	$scope.setArrowVisibility = function() {
-		setTimeout(function() {
-			$scope.showLeftArrow = false;
-			$scope.showRightArrow = !(document.querySelectorAll(".nav-inner")[0].clientWidth <= document.querySelectorAll(".nav-scroll")[0].clientWidth);
-			$scope.$apply();
-		}, 200);
-	}
-	$scope.setArrowVisibility();
+  $scope.node = {
+      'eth_mew' : {
+        'name'        : 'ETH',
+        'eip155'      : true,
+        'chainID'     : '1',
+        'tokenList'   : 'tokens-eth.js',
+        'estimateGas' : true,
+        'service'     : 'MyEtherWallet',
+        'url'         : '',
+        'port'        : ''
+      },
+      'etc_mew' : {
+        'name'        : 'ETC',
+        'eip155'      : false,
+        'chainID'     : false,
+        'tokenList'   : 'tokens-etc.js',
+        'estimateGas' : true,
+        'service'     : 'MyEtherWallet',
+        'url'         : '',
+        'port'        : ''
+      },
+      'tst_mew' : {
+        'name'        : 'Ropsten',
+        'eip155'      : true,
+        'chainID'     : '3',
+        'tokenList'   : 'tokens-rop.js',
+        'estimateGas' : true,
+        'service'     : 'MyEtherWallet',
+        'url'         : '',
+        'port'        : ''
+      },
+      'eth_ethscan' : {
+        'name'        : 'ETH',
+        'eip155'      : true,
+        'chainID'     : '1',
+        'tokenList'   : 'tokens-eth.js',
+        'estimateGas' : false,
+        'service'     : 'Etherscan.io',
+        'url'         : '',
+        'port'        : ''
+      },
+      'custom' : {
+        'name'        : 'Custom',
+        'eip155'      : '',
+        'chainID'     : '',
+        'tokenList'   : '',
+        'estimateGas' : false,
+        'service'     : 'Custom',
+        'url'         : '',
+        'port'        : ''
+      }
+  };
+  $scope.curNodeKey = 'eth_mew';
+  $scope.curNode = $scope.node.eth_mew;
 
-	$scope.setTab = function(hval) {
-		if (hval != "") {
-			hval = hval.replace("#", '');
-			for (var key in $scope.tabNames) {
-				if ($scope.tabNames[key].url == hval) {
-					$scope.activeTab = globalService.currentTab = $scope.tabNames[key].id;
-					break;
-				}
-				$scope.activeTab = globalService.currentTab;
-			}
-		} else {
-			$scope.activeTab = globalService.currentTab;
-		}
-	}
-	$scope.setTab(hval);
+  var hval = window.location.hash;
 
-	$scope.tabClick = function(id) {
-		$scope.activeTab = globalService.currentTab = id;
-		for (var key in $scope.tabNames) {
-			if ($scope.tabNames[key].id == id) location.hash = $scope.tabNames[key].url;
-		}
-	}
+  $scope.setArrowVisibility = function() {
+    setTimeout(function() {
+      $scope.showLeftArrow = false;
+      $scope.showRightArrow = !(document.querySelectorAll('.nav-inner')[0].clientWidth <= document.querySelectorAll('.nav-scroll')[0].clientWidth);
+      $scope.$apply();
+    }, 200);
+  }
+  $scope.setArrowVisibility();
 
-	$scope.setLanguageVal = function (id, varName, pos) {
-		$translate(id).then(function(paragraph) {
-			globalFuncs[varName][pos] = paragraph;
-		}, function(translationId) {
-			globalFuncs[varName][pos] = translationId;
-		});
-	}
+  $scope.changeNode = function(key) {
+    $scope.curNodeKey = key;
+    $scope.curNode = $scope.node[key];
+    $scope.dropdownNode = false;
+    localStorage.setItem('curNode', JSON.stringify({
+      key: key
+    }));
+  }
+  $scope.setNodeFromStorage = function() {
+    var node = localStorage.getItem('curNode');
+    if (node == null) {
+      $scope.changeNode($scope.curNodeKey);
+    } else {
+      node = JSON.parse(node);
+      var key = globalFuncs.stripTags(node.key);
+      $scope.changeNode(key);
+    }
+  }
+  $scope.setNodeFromStorage();
 
-	$scope.setErrorMsgLanguage = function() {
-		for (var i = 0; i < globalFuncs.errorMsgs.length; i++) $scope.setLanguageVal('ERROR_' + (i + 1), 'errorMsgs', i);
-		for (var i = 0; i < globalFuncs.successMsgs.length; i++) $scope.setLanguageVal('SUCCESS_' + (i + 1), 'successMsgs', i);
-	}
 
-	$scope.setGethErrMsgLanguage = function() {
-		globalFuncs.gethErrorMsgs = {};
-		for (var s in globalFuncs.gethErrors) {
-			var key = globalFuncs.gethErrors[s];
-			if (key.indexOf("GETH_") === 0) {
-				$scope.setLanguageVal(key,'gethErrorMsgs',key);
-			}
-		}
-	}
+  $scope.setTab = function(hval) {
+    if (hval != '') {
+      hval = hval.replace('#', '');
+      for (var key in $scope.tabNames) {
+        if ($scope.tabNames[key].url == hval) {
+          $scope.activeTab = globalService.currentTab = $scope.tabNames[key].id;
+          break;
+        }
+        $scope.activeTab = globalService.currentTab;
+      }
+    } else {
+      $scope.activeTab = globalService.currentTab;
+    }
+  }
+  $scope.setTab(hval);
 
-	$scope.setParityErrMsgLanguage = function() {
-		globalFuncs.parityErrorMsgs = {};
-		for (var s in globalFuncs.parityErrors) {
-			var key = globalFuncs.parityErrors[s];
-			if (key.indexOf("PARITY_") === 0) {
-				$scope.setLanguageVal(key,'parityErrorMsgs',key);
-			}
-		}
-	}
+  $scope.tabClick = function(id) {
+    $scope.activeTab = globalService.currentTab = id;
+    for (var key in $scope.tabNames) {
+      if ($scope.tabNames[key].id == id) location.hash = $scope.tabNames[key].url;
+    }
+  }
 
-	$scope.changeLanguage = function(key, value) {
-		$translate.use(key);
-		$scope.setErrorMsgLanguage();
-		if (globalFuncs.getEthNodeName() == "geth")
-			$scope.setGethErrMsgLanguage();
-		else
-			$scope.setParityErrMsgLanguage();
-		$scope.curLang = value;
-		$scope.setArrowVisibility();
-		$scope.dropdown = false;
-		localStorage.setItem("language", JSON.stringify({
-			key: key,
-			value: value
-		}));
-	}
+  $scope.setLanguageVal = function(id, varName, pos) {
+    $translate(id).then(function(paragraph) {
+      globalFuncs[varName][pos] = paragraph;
+    }, function(translationId) {
+      globalFuncs[varName][pos] = translationId;
+    });
+  }
 
-	$scope.changeNode = function(key, value) {
-		$scope.curNode = value;
-		$scope.setArrowVisibility();
-		$scope.dropdownNode = false;
-		localStorage.setItem("node", JSON.stringify({
-			key: key,
-			value: value
-		}));
-	}
+  $scope.setErrorMsgLanguage = function() {
+    for (var i = 0; i < globalFuncs.errorMsgs.length; i++) $scope.setLanguageVal('ERROR_' + (i + 1), 'errorMsgs', i);
+    for (var i = 0; i < globalFuncs.successMsgs.length; i++) $scope.setLanguageVal('SUCCESS_' + (i + 1), 'successMsgs', i);
+  }
 
-	$scope.setLanguageFromStorage = function() {
-		var lang = localStorage.getItem('language');
-		if (lang == null) return;
-		lang = JSON.parse(lang);
-		var key = globalFuncs.stripTags(lang.key);
-		var value = globalFuncs.stripTags(lang.value);
-		$scope.changeLanguage(key, value);
-	}
-	$scope.setLanguageFromStorage();
+  $scope.setGethErrMsgLanguage = function() {
+    globalFuncs.gethErrorMsgs = {};
+    for (var s in globalFuncs.gethErrors) {
+      var key = globalFuncs.gethErrors[s];
+      if (key.indexOf('GETH_') === 0) {
+        $scope.setLanguageVal(key, 'gethErrorMsgs', key);
+      }
+    }
+  }
 
-	$scope.setHash = function(hash) {
-		location.hash = hash;
-		$scope.setTab(hash);
-		$scope.$apply();
-	}
+  $scope.setParityErrMsgLanguage = function() {
+    globalFuncs.parityErrorMsgs = {};
+    for (var s in globalFuncs.parityErrors) {
+      var key = globalFuncs.parityErrors[s];
+      if (key.indexOf('PARITY_') === 0) {
+        $scope.setLanguageVal(key, 'parityErrorMsgs', key);
+      }
+    }
+  }
 
-	$scope.scrollHoverIn = function(isLeft, val) {
-		clearInterval($scope.sHoverTimer);
-		$scope.sHoverTimer = setInterval(function() {
-			if (isLeft) $scope.scrollLeft(val);
-			else $scope.scrollRight(val);
-		}, 20);
-	}
+  $scope.changeLanguage = function(key, value) {
+    $translate.use(key);
+    $scope.setErrorMsgLanguage();
+    if (globalFuncs.getEthNodeName() == 'geth')
+      $scope.setGethErrMsgLanguage();
+    else
+      $scope.setParityErrMsgLanguage();
+    $scope.curLang = value;
+    $scope.setArrowVisibility();
+    $scope.dropdown = false;
+    localStorage.setItem('language', JSON.stringify({
+      key: key,
+      value: value
+    }));
+  }
+  $scope.setLanguageFromStorage = function() {
+    var lang = localStorage.getItem('language');
+    if (lang == null) return;
+    lang = JSON.parse(lang);
+    var key = globalFuncs.stripTags(lang.key);
+    var value = globalFuncs.stripTags(lang.value);
+    $scope.changeLanguage(key, value);
+  }
+  $scope.setLanguageFromStorage();
 
-	$scope.scrollHoverOut = function() {
-		clearInterval($scope.sHoverTimer);
-	}
 
-  $scope.setOnScrollArrows = function(){
-    var ele = document.querySelectorAll(".nav-scroll")[0];
-  	$scope.showLeftArrow = ele.scrollLeft > 0;
-		$scope.showRightArrow = document.querySelectorAll(".nav-inner")[0].clientWidth > (ele.clientWidth + ele.scrollLeft);
+
+  $scope.setHash = function(hash) {
+    location.hash = hash;
+    $scope.setTab(hash);
     $scope.$apply();
   }
 
-	$scope.scrollLeft = function(val) {
-		var ele = document.querySelectorAll(".nav-scroll")[0];
-		ele.scrollLeft -= val;
-	}
+  $scope.scrollHoverIn = function(isLeft, val) {
+    clearInterval($scope.sHoverTimer);
+    $scope.sHoverTimer = setInterval(function() {
+      if (isLeft) $scope.scrollLeft(val);
+      else $scope.scrollRight(val);
+    }, 20);
+  }
 
-	$scope.scrollRight = function(val) {
-		var ele = document.querySelectorAll(".nav-scroll")[0];
-		ele.scrollLeft += val;
-	}
+  $scope.scrollHoverOut = function() {
+    clearInterval($scope.sHoverTimer);
+  }
 
-  angular.element(document.querySelectorAll(".nav-scroll")[0]).bind('scroll',$scope.setOnScrollArrows);
-	globalFuncs.changeHash = $scope.setHash;
+  $scope.setOnScrollArrows = function() {
+    var ele = document.querySelectorAll('.nav-scroll')[0];
+    $scope.showLeftArrow = ele.scrollLeft > 0;
+    $scope.showRightArrow = document.querySelectorAll('.nav-inner')[0].clientWidth > (ele.clientWidth + ele.scrollLeft);
+    $scope.$apply();
+  }
+
+  $scope.scrollLeft = function(val) {
+    var ele = document.querySelectorAll('.nav-scroll')[0];
+    ele.scrollLeft -= val;
+  }
+
+  $scope.scrollRight = function(val) {
+    var ele = document.querySelectorAll('.nav-scroll')[0];
+    ele.scrollLeft += val;
+  }
+
+  angular.element(document.querySelectorAll('.nav-scroll')[0]).bind('scroll', $scope.setOnScrollArrows);
+  globalFuncs.changeHash = $scope.setHash;
 
 };
 module.exports = tabsCtrl;
--- dist/js/etherwallet-master.js
+++ dist/js/etherwallet-master.js
@@ -1469,145 +1469,209 @@ module.exports = signMsgCtrl;
 'use strict';
 
 var tabsCtrl = function ($scope, globalService, $translate) {
-	$scope.tabNames = globalService.tabs;
-	$scope.curLang = "English";
-	$scope.curNode = "Select Node"; //todo = fetch from localstorage
-	var hval = window.location.hash;
+  $scope.tabNames = globalService.tabs;
+  $scope.curLang = 'English';
 
-	$scope.setArrowVisibility = function () {
-		setTimeout(function () {
-			$scope.showLeftArrow = false;
-			$scope.showRightArrow = !(document.querySelectorAll(".nav-inner")[0].clientWidth <= document.querySelectorAll(".nav-scroll")[0].clientWidth);
-			$scope.$apply();
-		}, 200);
-	};
-	$scope.setArrowVisibility();
+  $scope.node = {
+    'eth_mew': {
+      'name': 'ETH',
+      'eip155': true,
+      'chainID': '1',
+      'tokenList': 'tokens-eth.js',
+      'estimateGas': true,
+      'service': 'MyEtherWallet',
+      'url': '',
+      'port': ''
+    },
+    'etc_mew': {
+      'name': 'ETC',
+      'eip155': false,
+      'chainID': false,
+      'tokenList': 'tokens-etc.js',
+      'estimateGas': true,
+      'service': 'MyEtherWallet',
+      'url': '',
+      'port': ''
+    },
+    'tst_mew': {
+      'name': 'Ropsten',
+      'eip155': true,
+      'chainID': '3',
+      'tokenList': 'tokens-rop.js',
+      'estimateGas': true,
+      'service': 'MyEtherWallet',
+      'url': '',
+      'port': ''
+    },
+    'eth_ethscan': {
+      'name': 'ETH',
+      'eip155': true,
+      'chainID': '1',
+      'tokenList': 'tokens-eth.js',
+      'estimateGas': false,
+      'service': 'Etherscan.io',
+      'url': '',
+      'port': ''
+    },
+    'custom': {
+      'name': 'Custom',
+      'eip155': '',
+      'chainID': '',
+      'tokenList': '',
+      'estimateGas': false,
+      'service': 'Custom',
+      'url': '',
+      'port': ''
+    }
+  };
+  $scope.curNodeKey = 'eth_mew';
+  $scope.curNode = $scope.node.eth_mew;
 
-	$scope.setTab = function (hval) {
-		if (hval != "") {
-			hval = hval.replace("#", '');
-			for (var key in $scope.tabNames) {
-				if ($scope.tabNames[key].url == hval) {
-					$scope.activeTab = globalService.currentTab = $scope.tabNames[key].id;
-					break;
-				}
-				$scope.activeTab = globalService.currentTab;
-			}
-		} else {
-			$scope.activeTab = globalService.currentTab;
-		}
-	};
-	$scope.setTab(hval);
+  var hval = window.location.hash;
 
-	$scope.tabClick = function (id) {
-		$scope.activeTab = globalService.currentTab = id;
-		for (var key in $scope.tabNames) {
-			if ($scope.tabNames[key].id == id) location.hash = $scope.tabNames[key].url;
-		}
-	};
+  $scope.setArrowVisibility = function () {
+    setTimeout(function () {
+      $scope.showLeftArrow = false;
+      $scope.showRightArrow = !(document.querySelectorAll('.nav-inner')[0].clientWidth <= document.querySelectorAll('.nav-scroll')[0].clientWidth);
+      $scope.$apply();
+    }, 200);
+  };
+  $scope.setArrowVisibility();
 
-	$scope.setLanguageVal = function (id, varName, pos) {
-		$translate(id).then(function (paragraph) {
-			globalFuncs[varName][pos] = paragraph;
-		}, function (translationId) {
-			globalFuncs[varName][pos] = translationId;
-		});
-	};
+  $scope.changeNode = function (key) {
+    $scope.curNodeKey = key;
+    $scope.curNode = $scope.node[key];
+    $scope.dropdownNode = false;
+    localStorage.setItem('curNode', JSON.stringify({
+      key: key
+    }));
+  };
+  $scope.setNodeFromStorage = function () {
+    var node = localStorage.getItem('curNode');
+    if (node == null) {
+      $scope.changeNode($scope.curNodeKey);
+    } else {
+      node = JSON.parse(node);
+      var key = globalFuncs.stripTags(node.key);
+      $scope.changeNode(key);
+    }
+  };
+  $scope.setNodeFromStorage();
 
-	$scope.setErrorMsgLanguage = function () {
-		for (var i = 0; i < globalFuncs.errorMsgs.length; i++) $scope.setLanguageVal('ERROR_' + (i + 1), 'errorMsgs', i);
-		for (var i = 0; i < globalFuncs.successMsgs.length; i++) $scope.setLanguageVal('SUCCESS_' + (i + 1), 'successMsgs', i);
-	};
+  $scope.setTab = function (hval) {
+    if (hval != '') {
+      hval = hval.replace('#', '');
+      for (var key in $scope.tabNames) {
+        if ($scope.tabNames[key].url == hval) {
+          $scope.activeTab = globalService.currentTab = $scope.tabNames[key].id;
+          break;
+        }
+        $scope.activeTab = globalService.currentTab;
+      }
+    } else {
+      $scope.activeTab = globalService.currentTab;
+    }
+  };
+  $scope.setTab(hval);
 
-	$scope.setGethErrMsgLanguage = function () {
-		globalFuncs.gethErrorMsgs = {};
-		for (var s in globalFuncs.gethErrors) {
-			var key = globalFuncs.gethErrors[s];
-			if (key.indexOf("GETH_") === 0) {
-				$scope.setLanguageVal(key, 'gethErrorMsgs', key);
-			}
-		}
-	};
+  $scope.tabClick = function (id) {
+    $scope.activeTab = globalService.currentTab = id;
+    for (var key in $scope.tabNames) {
+      if ($scope.tabNames[key].id == id) location.hash = $scope.tabNames[key].url;
+    }
+  };
 
-	$scope.setParityErrMsgLanguage = function () {
-		globalFuncs.parityErrorMsgs = {};
-		for (var s in globalFuncs.parityErrors) {
-			var key = globalFuncs.parityErrors[s];
-			if (key.indexOf("PARITY_") === 0) {
-				$scope.setLanguageVal(key, 'parityErrorMsgs', key);
-			}
-		}
-	};
+  $scope.setLanguageVal = function (id, varName, pos) {
+    $translate(id).then(function (paragraph) {
+      globalFuncs[varName][pos] = paragraph;
+    }, function (translationId) {
+      globalFuncs[varName][pos] = translationId;
+    });
+  };
 
-	$scope.changeLanguage = function (key, value) {
-		$translate.use(key);
-		$scope.setErrorMsgLanguage();
-		if (globalFuncs.getEthNodeName() == "geth") $scope.setGethErrMsgLanguage();else $scope.setParityErrMsgLanguage();
-		$scope.curLang = value;
-		$scope.setArrowVisibility();
-		$scope.dropdown = false;
-		localStorage.setItem("language", JSON.stringify({
-			key: key,
-			value: value
-		}));
-	};
+  $scope.setErrorMsgLanguage = function () {
+    for (var i = 0; i < globalFuncs.errorMsgs.length; i++) $scope.setLanguageVal('ERROR_' + (i + 1), 'errorMsgs', i);
+    for (var i = 0; i < globalFuncs.successMsgs.length; i++) $scope.setLanguageVal('SUCCESS_' + (i + 1), 'successMsgs', i);
+  };
 
-	$scope.changeNode = function (key, value) {
-		$scope.curNode = value;
-		$scope.setArrowVisibility();
-		$scope.dropdownNode = false;
-		localStorage.setItem("node", JSON.stringify({
-			key: key,
-			value: value
-		}));
-	};
+  $scope.setGethErrMsgLanguage = function () {
+    globalFuncs.gethErrorMsgs = {};
+    for (var s in globalFuncs.gethErrors) {
+      var key = globalFuncs.gethErrors[s];
+      if (key.indexOf('GETH_') === 0) {
+        $scope.setLanguageVal(key, 'gethErrorMsgs', key);
+      }
+    }
+  };
 
-	$scope.setLanguageFromStorage = function () {
-		var lang = localStorage.getItem('language');
-		if (lang == null) return;
-		lang = JSON.parse(lang);
-		var key = globalFuncs.stripTags(lang.key);
-		var value = globalFuncs.stripTags(lang.value);
-		$scope.changeLanguage(key, value);
-	};
-	$scope.setLanguageFromStorage();
+  $scope.setParityErrMsgLanguage = function () {
+    globalFuncs.parityErrorMsgs = {};
+    for (var s in globalFuncs.parityErrors) {
+      var key = globalFuncs.parityErrors[s];
+      if (key.indexOf('PARITY_') === 0) {
+        $scope.setLanguageVal(key, 'parityErrorMsgs', key);
+      }
+    }
+  };
 
-	$scope.setHash = function (hash) {
-		location.hash = hash;
-		$scope.setTab(hash);
-		$scope.$apply();
-	};
+  $scope.changeLanguage = function (key, value) {
+    $translate.use(key);
+    $scope.setErrorMsgLanguage();
+    if (globalFuncs.getEthNodeName() == 'geth') $scope.setGethErrMsgLanguage();else $scope.setParityErrMsgLanguage();
+    $scope.curLang = value;
+    $scope.setArrowVisibility();
+    $scope.dropdown = false;
+    localStorage.setItem('language', JSON.stringify({
+      key: key,
+      value: value
+    }));
+  };
+  $scope.setLanguageFromStorage = function () {
+    var lang = localStorage.getItem('language');
+    if (lang == null) return;
+    lang = JSON.parse(lang);
+    var key = globalFuncs.stripTags(lang.key);
+    var value = globalFuncs.stripTags(lang.value);
+    $scope.changeLanguage(key, value);
+  };
+  $scope.setLanguageFromStorage();
 
-	$scope.scrollHoverIn = function (isLeft, val) {
-		clearInterval($scope.sHoverTimer);
-		$scope.sHoverTimer = setInterval(function () {
-			if (isLeft) $scope.scrollLeft(val);else $scope.scrollRight(val);
-		}, 20);
-	};
+  $scope.setHash = function (hash) {
+    location.hash = hash;
+    $scope.setTab(hash);
+    $scope.$apply();
+  };
 
-	$scope.scrollHoverOut = function () {
-		clearInterval($scope.sHoverTimer);
-	};
+  $scope.scrollHoverIn = function (isLeft, val) {
+    clearInterval($scope.sHoverTimer);
+    $scope.sHoverTimer = setInterval(function () {
+      if (isLeft) $scope.scrollLeft(val);else $scope.scrollRight(val);
+    }, 20);
+  };
 
-	$scope.setOnScrollArrows = function () {
-		var ele = document.querySelectorAll(".nav-scroll")[0];
-		$scope.showLeftArrow = ele.scrollLeft > 0;
-		$scope.showRightArrow = document.querySelectorAll(".nav-inner")[0].clientWidth > ele.clientWidth + ele.scrollLeft;
-		$scope.$apply();
-	};
+  $scope.scrollHoverOut = function () {
+    clearInterval($scope.sHoverTimer);
+  };
 
-	$scope.scrollLeft = function (val) {
-		var ele = document.querySelectorAll(".nav-scroll")[0];
-		ele.scrollLeft -= val;
-	};
+  $scope.setOnScrollArrows = function () {
+    var ele = document.querySelectorAll('.nav-scroll')[0];
+    $scope.showLeftArrow = ele.scrollLeft > 0;
+    $scope.showRightArrow = document.querySelectorAll('.nav-inner')[0].clientWidth > ele.clientWidth + ele.scrollLeft;
+    $scope.$apply();
+  };
 
-	$scope.scrollRight = function (val) {
-		var ele = document.querySelectorAll(".nav-scroll")[0];
-		ele.scrollLeft += val;
-	};
+  $scope.scrollLeft = function (val) {
+    var ele = document.querySelectorAll('.nav-scroll')[0];
+    ele.scrollLeft -= val;
+  };
 
-	angular.element(document.querySelectorAll(".nav-scroll")[0]).bind('scroll', $scope.setOnScrollArrows);
-	globalFuncs.changeHash = $scope.setHash;
+  $scope.scrollRight = function (val) {
+    var ele = document.querySelectorAll('.nav-scroll')[0];
+    ele.scrollLeft += val;
+  };
+
+  angular.element(document.querySelectorAll('.nav-scroll')[0]).bind('scroll', $scope.setOnScrollArrows);
+  globalFuncs.changeHash = $scope.setHash;
 };
 module.exports = tabsCtrl;
 
@@ -1919,38 +1983,38 @@ module.exports = walletBalanceCtrl;
 'use strict';
 
 var walletGenCtrl = function ($scope) {
-	$scope.password = "";
-	$scope.wallet = null;
-	$scope.showWallet = false;
-	$scope.blob = $scope.blobEnc = "";
-	$scope.isDone = true;
-	$scope.showPass = true;
-	$scope.genNewWallet = function () {
-		if (!$scope.isStrongPass()) {
-			alert(globalFuncs.errorMsgs[1]);
-		} else if ($scope.isDone) {
-			$scope.isDone = false;
-			$scope.wallet = Wallet.generate(false);
-			$scope.showWallet = true;
-			$scope.blob = globalFuncs.getBlob("text/json;charset=UTF-8", $scope.wallet.toJSON());
-			$scope.blobEnc = globalFuncs.getBlob("text/json;charset=UTF-8", $scope.wallet.toV3($scope.password, {
-				kdf: globalFuncs.kdf,
-				n: globalFuncs.scrypt.n
-			}));
-			$scope.encFileName = $scope.wallet.getV3Filename();
-			if (parent != null) parent.postMessage(JSON.stringify({ address: $scope.wallet.getAddressString(), checksumAddress: $scope.wallet.getChecksumAddressString() }), "*");
-			$scope.isDone = true;
-		}
-	};
-	$scope.printQRCode = function () {
-		globalFuncs.printPaperWallets(JSON.stringify([{
-			address: $scope.wallet.getAddressString(),
-			private: $scope.wallet.getPrivateKeyString()
-		}]));
-	};
-	$scope.isStrongPass = function () {
-		return globalFuncs.isStrongPass($scope.password);
-	};
+  $scope.password = "";
+  $scope.wallet = null;
+  $scope.showWallet = false;
+  $scope.blob = $scope.blobEnc = "";
+  $scope.isDone = true;
+  $scope.showPass = true;
+  $scope.genNewWallet = function () {
+    if (!$scope.isStrongPass()) {
+      alert(globalFuncs.errorMsgs[1]);
+    } else if ($scope.isDone) {
+      $scope.isDone = false;
+      $scope.wallet = Wallet.generate(false);
+      $scope.showWallet = true;
+      $scope.blob = globalFuncs.getBlob("text/json;charset=UTF-8", $scope.wallet.toJSON());
+      $scope.blobEnc = globalFuncs.getBlob("text/json;charset=UTF-8", $scope.wallet.toV3($scope.password, {
+        kdf: globalFuncs.kdf,
+        n: globalFuncs.scrypt.n
+      }));
+      $scope.encFileName = $scope.wallet.getV3Filename();
+      if (parent != null) parent.postMessage(JSON.stringify({ address: $scope.wallet.getAddressString(), checksumAddress: $scope.wallet.getChecksumAddressString() }), "*");
+      $scope.isDone = true;
+    }
+  };
+  $scope.printQRCode = function () {
+    globalFuncs.printPaperWallets(JSON.stringify([{
+      address: $scope.wallet.getAddressString(),
+      private: $scope.wallet.getPrivateKeyString()
+    }]));
+  };
+  $scope.isStrongPass = function () {
+    return globalFuncs.isStrongPass($scope.password);
+  };
 };
 module.exports = walletGenCtrl;
 
