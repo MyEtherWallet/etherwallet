8db42e662c1b9069aff7d6d4a20fe3c8986d391a
kvhnuke <kvhnuke@aol.com>
2017-01-02 18:42:27 -0800

    added etherscan api key

--- chrome-extension/js/etherwallet-master.js
+++ chrome-extension/js/etherwallet-master.js
@@ -3049,6 +3049,7 @@ etherscan.queuePost = function () {
     var data = this.pendingPosts[0].data;
     var callback = this.pendingPosts[0].callback;
     var parentObj = this;
+    data.apikey = 'DSH5B24BQYKD1AD8KUCDY3SAQSS6ZAU175';
     ajaxReq.http.post(this.SERVERURL, ajaxReq.postSerializer(data), this.config).then(function (data) {
         callback(data.data);
         parentObj.pendingPosts.splice(0, 1);
