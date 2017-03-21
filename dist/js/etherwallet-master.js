dc6dbdbeff7842353a8bef87b1867b22e66963e6
kvhnuke <kvhnuke@aol.com>
2017-03-21 01:10:51 -0700

    added test to verify wallet generation

--- dist/js/etherwallet-master.js
+++ dist/js/etherwallet-master.js
@@ -2181,39 +2181,40 @@ module.exports = walletBalanceCtrl;
 'use strict';
 
 var walletGenCtrl = function ($scope) {
-  $scope.password = "";
-  $scope.wallet = null;
-  $scope.showWallet = false;
-  $scope.blob = $scope.blobEnc = "";
-  $scope.isDone = true;
-  $scope.showPass = true;
-  $scope.genNewWallet = function () {
-    if (!$scope.isStrongPass()) {
-      $scope.notifier.danger(globalFuncs.errorMsgs[1]);
-    } else if ($scope.isDone) {
-      $scope.isDone = false;
-      $scope.wallet = Wallet.generate(false);
-      $scope.showWallet = true;
-      $scope.blob = globalFuncs.getBlob("text/json;charset=UTF-8", $scope.wallet.toJSON());
-      $scope.blobEnc = globalFuncs.getBlob("text/json;charset=UTF-8", $scope.wallet.toV3($scope.password, {
-        kdf: globalFuncs.kdf,
-        n: globalFuncs.scrypt.n
-      }));
-      $scope.notifier.warning(globalFuncs.errorMsgs[28]);
-      $scope.encFileName = $scope.wallet.getV3Filename();
-      if (parent != null) parent.postMessage(JSON.stringify({ address: $scope.wallet.getAddressString(), checksumAddress: $scope.wallet.getChecksumAddressString() }), "*");
-      $scope.isDone = true;
-    }
-  };
-  $scope.printQRCode = function () {
-    globalFuncs.printPaperWallets(JSON.stringify([{
-      address: $scope.wallet.getAddressString(),
-      private: $scope.wallet.getPrivateKeyString()
-    }]));
-  };
-  $scope.isStrongPass = function () {
-    return globalFuncs.isStrongPass($scope.password);
-  };
+    $scope.password = "";
+    $scope.wallet = null;
+    $scope.showWallet = false;
+    $scope.blob = $scope.blobEnc = "";
+    $scope.isDone = true;
+    $scope.showPass = true;
+    $scope.genNewWallet = function () {
+        if (!$scope.isStrongPass()) {
+            $scope.notifier.danger(globalFuncs.errorMsgs[1]);
+        } else if ($scope.isDone) {
+            $scope.isDone = false;
+            $scope.wallet = Wallet.generate(false);
+            $scope.showWallet = true;
+            $scope.blob = globalFuncs.getBlob("text/json;charset=UTF-8", $scope.wallet.toJSON());
+            $scope.blobEnc = globalFuncs.getBlob("text/json;charset=UTF-8", $scope.wallet.toV3($scope.password, {
+                kdf: globalFuncs.kdf,
+                n: globalFuncs.scrypt.n
+            }));
+            $scope.notifier.warning(globalFuncs.errorMsgs[28]);
+            $scope.encFileName = $scope.wallet.getV3Filename();
+            if (parent != null) parent.postMessage(JSON.stringify({ address: $scope.wallet.getAddressString(), checksumAddress: $scope.wallet.getChecksumAddressString() }), "*");
+            $scope.isDone = true;
+            if (!$scope.$$phase) $scope.$apply();
+        }
+    };
+    $scope.printQRCode = function () {
+        globalFuncs.printPaperWallets(JSON.stringify([{
+            address: $scope.wallet.getAddressString(),
+            private: $scope.wallet.getPrivateKeyString()
+        }]));
+    };
+    $scope.isStrongPass = function () {
+        return globalFuncs.isStrongPass($scope.password);
+    };
 };
 module.exports = walletGenCtrl;
 
