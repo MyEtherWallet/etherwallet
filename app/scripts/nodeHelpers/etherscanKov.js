34024eb4d4e225767387ef0a185a8428bfb3ba24
kvhnuke <kvhnuke@aol.com>
2017-04-17 18:27:27 -0700

    clone etherscan obj

--- app/scripts/nodeHelpers/etherscanKov.js
+++ app/scripts/nodeHelpers/etherscanKov.js
@@ -1,4 +1,8 @@
 'use strict';
-var kovan = require('./etherscan');
+var _ethscan = require('./etherscan');
+var kovan = {};
+for (var attr in _ethscan) {
+    kovan[attr] = _ethscan[attr];
+}
 kovan.SERVERURL = 'https://kovan.etherscan.io/api';
 module.exports = kovan;
--- app/scripts/nodeHelpers/etherscanRin.js
+++ app/scripts/nodeHelpers/etherscanRin.js
@@ -1,4 +1,8 @@
 'use strict';
-var rinkeby = require('./etherscan');
+var _ethscan = require('./etherscan');
+var rinkeby = {};
+for (var attr in _ethscan) {
+    rinkeby[attr] = _ethscan[attr];
+}
 rinkeby.SERVERURL = 'https://rinkeby.etherscan.io/api';
 module.exports = rinkeby;
