6012a5c7f66b2614b5f7f4176afaf4f3ea53f8ee
tayvano <tayvano@gmail.com>
2017-04-30 04:00:09 -0700

    clean up balances a bit

--- app/includes/myWallet.tpl
+++ app/includes/myWallet.tpl
@@ -14,13 +14,18 @@
         <span class="mono small">{{twallet.addr}}</span>
       </td>
       <td class="chrome-tokens">
-        <h3 class="text-success col-xs-12">{{twallet.balance }} ETH</h3>
-        <div class="small col-xs-6" ng-repeat="token in twallet.tokens" ng-show="token.balance!=0 && token.balance!='loading'"><strong>{{token.getBalance() }} </strong> {{token.getSymbol()}} &nbsp;&nbsp; </div>
+        <h3 class="text-success col-xs-12 point" ng-show="!showLongBal" ng-dblclick="showLongBal=!showLongBal" title="{{twallet.balance}} (Double-Click)" >{{twallet.balance|number}} ETH</h3>
+        <h3 class="text-success col-xs-12 point" ng-show="showLongBal" ng-dblclick="showLongBal=!showLongBal">{{twallet.balance }} ETH</h3>
+        <div class="small col-xs-6 point" ng-repeat="token in twallet.tokens" ng-show="token.balance!=0 && token.balance!='loading'" ng-dblclick="showLongToken=!showLongToken" title="{{token.getBalance()}} (Double-Click)">
+          <strong ng-show="!showLongToken">{{token.getBalance()|number}} </strong>
+          <strong ng-show="showLongToken"> {{token.getBalance()}} </strong>
+          {{token.getSymbol()}} &nbsp;&nbsp;
+        </div>
       </td>
       <td class="text-center">
         <a class="mainWalletEdit" ng-click="editMWallet($index,'wallet')"><img src="images/icon-edit.svg" title="Edit" /></a>
         <a class="text-warning mainWalletView" ng-click="viewMWallet($index,'wallet')"><img src="images/icon-view.svg" title="View" /></a>
-        <a class="mainWalletDelete text-danger" ng-click="deleteWalletMsg($index,'wallet')"><img src="images/icon-remove.svg" title="Remove" /></a>
+        <a class="mainWalletDelete text-danger" ng-click="deleteWalletMsg($index,'wallet')"><img src="images/icon-remove-red.svg" title="Remove" /></a>
       </td>
     </tr>
   </table>
@@ -38,10 +43,16 @@
           <span class="mono small"> {{twallet.addr}} </span>
         </td>
         <td class="chrome-tokens">
-          <h3 class="text-success col-xs-12">{{twallet.balance }} ETH</h3>
-          <div class="small col-xs-6" ng-repeat="token in twallet.tokens" ng-show="token.balance!=0 && token.balance!='loading'"><strong>{{token.getBalance() }}</strong>&nbsp;{{token.getSymbol()}} &nbsp;&nbsp;</div>
+          <h3 class="text-success col-xs-12 point" ng-show="!showLongBal" ng-dblclick="showLongBal=!showLongBal" title="{{twallet.balance}} (Double-Click)" >{{twallet.balance|number}} ETH</h3>
+          <h3 class="text-success col-xs-12 point" ng-show="showLongBal" ng-dblclick="showLongBal=!showLongBal">{{twallet.balance }} ETH</h3>
+
+          <div class="small col-xs-6 point" ng-repeat="token in twallet.tokens" ng-show="token.balance!=0 && token.balance!='loading'" ng-dblclick="showLongToken=!showLongToken" title="{{token.getBalance()}} (Double-Click)">
+            <strong ng-show="!showLongToken">{{token.getBalance()|number}} </strong>
+            <strong ng-show="showLongToken"> {{token.getBalance()}} </strong>
+            &nbsp;{{token.getSymbol()}} &nbsp;&nbsp;
+          </div>
         </td>
-         <td class="text-center"><a class="mainWalletDelete text-danger" ng-click="deleteWalletMsg($index,'watchOnly')"><img src="images/icon-remove.svg" title="Remove" /></a></td>
+         <td class="text-center"><a class="mainWalletDelete text-danger" ng-click="deleteWalletMsg($index,'watchOnly')"><img src="images/icon-remove-red.svg" title="Remove" /></a></td>
       </tr>
     </table>
   </section>
--- app/scripts/directives/balanceDrtv.js
+++ app/scripts/directives/balanceDrtv.js
@@ -1,68 +1,9 @@
 'use strict';
+
 var balanceDrtv = function() {
 	return {
         restrict : "E",
-        template : '<aside ng-controller=\'walletBalanceCtrl\'>\n\
-                      <h5 translate=\"sidebar_AccountAddr\">Account Address:</h5>\n\
-                      <ul class=\"account-info\">\n\
-                        <div class=\"addressIdenticon med float\" title=\"Address Indenticon\" blockie-address=\"{{wallet.getAddressString()}}\" watch-var=\"wallet\"></div>\n\
-                        <span class=\"mono wrap\">{{wallet.getChecksumAddressString()}}</span>\n\
-                      </ul>\n\
-                      <hr />\n\
-                      <h5 translate=\"sidebar_AccountBal\">Account Balance:</h5>\n\
-                      <ul class=\"account-info\">\n\
-                        <li><span class=\"mono wrap\">{{wallet.balance}}</span> {{ajaxReq.type}}</li>\n\
-                      </ul>\n\
-                      <section class=\"token-balances\">\n\
-                        <h5 translate=\"sidebar_TokenBal\">Token Balances:</h5>\n\
-                        <table class=\"account-info\">\n\
-                          <tr ng-repeat=\"token in wallet.tokenObjs track by $index\" ng-show=\"token.balance!=0 \&\& token.balance!=\'loading\' || token.type!==\'default\' || tokenVisibility==\'shown\'\">\n\
-                            <td class=\"mono wrap\">\n\
-                              <img src=\"images/icon-remove.svg\" class=\"token-remove\" title=\"Remove Token\" ng-click=\"removeTokenFromLocal(token.symbol)\" ng-show=\"token.type!==\'default\'\" />\n\
-                              {{token.getBalance()}}\n\
-                            </td>\n\
-                            <td>{{token.getSymbol()}} </td>\n\
-                          </tr>\n\
-                        </table>\n\
-                        <a class=\"btn btn-default btn-sm\" ng-click=\"tokenVisibility=\'shown\'\" ng-show=\"tokenVisibility==\'hidden\'\">Show All Tokens</a>\n\
-                        <a class=\"btn btn-default btn-sm\" ng-click=\"tokenVisibility=\'hidden\'\" ng-show=\"tokenVisibility==\'shown\'\">Hide Tokens</a>\n\
-                        <a class=\"btn btn-default btn-sm\" ng-click=\"customTokenField=!customTokenField\"><span translate=\"SEND_custom\"translate=\"SEND_custom\">Add Custom Token<\/span><\/a>\n \
-                        <div class=\"custom-token-fields\" ng-show=\"customTokenField\">\n\
-                          <label translate=\"TOKEN_Addr\">Address:</label>\n\
-                          <input class=\"form-control input-sm\" type=\"text\" ng-model=\"localToken.contractAdd\" ng-class=\"Validator.isValidAddress(localToken.contractAdd) ? \'is-valid\' : \'is-invalid\'\" />\n\
-                          <label translate=\"TOKEN_Symbol\">Token Symbol:</label>\n\
-                          <input class=\"form-control input-sm\" type=\"text\" ng-model=\"localToken.symbol\" ng-class=\"localToken.symbol!=\'\' ? \'is-valid\' : \'is-invalid\'\" />\n\
-                          <label translate=\"TOKEN_Dec\"> Decimals:</label>\n\
-                          <input class=\"form-control input-sm\" type=\"text\" ng-model=\"localToken.decimals\" ng-class=\"Validator.isPositiveNumber(localToken.decimals) ? \'is-valid\' : \'is-invalid\'\" />\n\
-                          <div class=\"btn btn-primary btn-sm\" ng-click=\"saveTokenToLocal()\" translate=\"x_Save\">Save</div>\n\
-                          <div ng-bind-html=\"validateLocalToken\"></div>\n\
-                        </div>\n\
-                      </section>\n\
-                      <hr />\n\
-                      <h5 translate=\"sidebar_TransHistory\"> Transaction History: </h5>\n\
-                      <ul class=\"account-info\">\n\
-                        <li ng-show=\"ajaxReq.type != \'CUS\'\">\n\
-                          <a href=\"{{ajaxReq.blockExplorerAddr.replace(\'[[address]]\', wallet.getAddressString())}}\" target=\"_blank\">\n\
-                            {{ajaxReq.type}} ({{ajaxReq.blockExplorerTX.replace(\'/tx/[[txHash]]\', \'\')}})\n\
-                          </a>\n\
-                        </li>\n\
-                        <li>\n\
-                          <a href=\"https://ethplorer.io/address/{{wallet.getAddressString()}}\" target=\"_blank\"> Tokens (Ethplorer.io) </a>\n\
-                        </li>\n\
-                      </ul>\n\
-                      <hr />\n\
-                      <section ng-show=\"ajaxReq.type==\'ETH\'\">\n\
-                        <h5 translate=\"sidebar_Equiv\">Equivalent Values:</h5>\n\
-                        <ul class=\"account-info\">\n\
-                          <li><span class=\"mono wrap\">{{wallet.btcBalance}}</span> BTC</li>\n\
-                          <li><span class=\"mono wrap\">{{wallet.repBalance}}</span> REP</li>\n\
-                          <li><span class=\"mono wrap\">{{wallet.eurBalance}}</span> EUR</li>\n\
-                          <li><span class=\"mono wrap\">{{wallet.chfBalance}}</span> CHF</li>\n\
-                          <li><span class=\"mono wrap\">{{wallet.usdBalance}}</span> USD</li>\n\
-                        </ul>\n\
-                        <a target="_blank" ng-click=\"globalService.currentTab=globalService.tabs.swap.id\" class=\"btn btn-primary btn-sm\">Swap via Bity</a>\n\
-                      </section>\n\
-                    </aside>'
+        template : require('./balanceDrtv.html')
   };
 };
 module.exports = balanceDrtv;
