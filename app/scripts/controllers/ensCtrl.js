9b042e2c694152321dcd7cb1305a4b1be6b7c118
kvhnuke <kvhnuke@aol.com>
2017-05-10 11:40:21 -0700

    no bidding if you dont have enough balance

--- app/scripts/controllers/ensCtrl.js
+++ app/scripts/controllers/ensCtrl.js
@@ -264,6 +264,7 @@ var ensCtrl = function($scope, $sce, walletService) {
             else if (_objENS.status != $scope.ensModes.reveal && (!$scope.Validator.isPositiveNumber(_objENS.dValue) || _objENS.dValue < _objENS.bidValue)) throw globalFuncs.errorMsgs[0];
             else if (!$scope.Validator.isPasswordLenValid(_objENS.secret, 0)) throw globalFuncs.errorMsgs[31];
             else if (_objENS.revealObject && _objENS.revealObject.name && ens.normalise(_objENS.revealObject.name) != _objENS.name) throw globalFuncs.errorMsgs[34];
+            else if ($scope.wallet.balance <= _objENS.dValue) throw globalFuncs.errorMsgs[0];
             else {
                 if ($scope.objENS.status == $scope.ensModes.open) $scope.openAndBidAuction();
                 else if ($scope.objENS.status == $scope.ensModes.auction) $scope.bidAuction();
