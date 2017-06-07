8ae0384adb54a8fe7f15d49011aca1fa0162076d
crptm <crptm@users.noreply.github.com>
2017-06-08 02:27:01 +0400

    extract custom gas messages to file

---
+++ app/scripts/customGas.js
@@ -0,0 +1,21 @@
+module.exports = [{ // donation address example
+    to: '0x7cB57B5A97eAbe94205C07890BE4c1aD31E486A8',
+    gasLimit: 21000,
+    data: '',
+    msg: 'Thank you for donating to MyEtherWallet. TO THE MOON!'
+}, { // BAT
+    to: '0x0D8775F648430679A709E98d2b0Cb6250d2887EF',
+    gasLimit: 200000,
+    data: '0xb4427263',
+    msg: 'BAT. THE SALE IS OVER. STOP CLOGGING THE BLOCKCHAIN PLEASE'
+}, { // BANCOR
+    to: '0x00000',
+    gasLimit: 200000,
+    data: '',
+    msg: 'Bancor. Starts June XX, 2017.'
+}, { // Moeda
+    to: '0x4870E705a3def9DDa6da7A953D1cd3CCEDD08573',
+    gasLimit: 200000,
+    data: '',
+    msg: 'Moeda. Ends at block 4,111,557.'
+}]
