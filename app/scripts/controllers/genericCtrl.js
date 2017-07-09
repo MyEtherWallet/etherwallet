5951a008a55b8c3ddbe4b4c5b56ee2b92a6549e2
Cody Burns <cody.w.burns@gmail.com>
2017-07-09 12:34:21 -0500

    Welcome msg
    change the message and timeout in genericCtl to whatcha need

--- app/scripts/controllers/genericCtrl.js
+++ app/scripts/controllers/genericCtrl.js
@@ -4,5 +4,19 @@ var genericCtrl = function($scope, globalService, $sce) {
     $scope.notifier = uiFuncs.notifier;
     $scope.notifier.sce = $sce;
     $scope.notifier.scope = $scope;
+
+    $scope.showLoading = true;
+   setTimeout(function ()
+   {
+     $scope.$apply(function()
+     {
+       $scope.showLoading = false;
+     });
+   }, 5000);
+
+    $scope.loadingMsg = {
+        msg : 'ETC is best ether!', //<----- change me
+        website : window.location.href
+      };
 };
 module.exports = genericCtrl;
--- dist/js/etherwallet-master.js
+++ dist/js/etherwallet-master.js
@@ -861,6 +861,18 @@ var genericCtrl = function ($scope, globalService, $sce) {
     $scope.notifier = uiFuncs.notifier;
     $scope.notifier.sce = $sce;
     $scope.notifier.scope = $scope;
+
+    $scope.showLoading = true;
+    setTimeout(function () {
+        $scope.$apply(function () {
+            $scope.showLoading = false;
+        });
+    }, 5000);
+
+    $scope.loadingMsg = {
+        msg: 'ETC is best ether!', //<----- change me
+        website: window.location.href
+    };
 };
 module.exports = genericCtrl;
 
