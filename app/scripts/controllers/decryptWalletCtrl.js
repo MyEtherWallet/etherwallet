c9c815609c399da2da3c6b321046892fce717775
danbaragan <daniel.mihai.baragan@gmail.com>
2017-10-11 14:49:20 +0300

    Add SingularDTV derivation to mnemonic paths.
    The HD wallet path for singular is differrent from all the rest, user is
    required to always enter a custom path to get to his/hers SNGLS tokens
    from a SingularDTV Lightwallet generated mnemonic.
    The reason for this is that SingularDTV launched it's wallet before
    `m/44'/60'/...` became standard.
    Since a lot of users keep asking about the singular derivation path
    when used in conjuction with MyEtherWallet, adding this path to the options
    will help quite a few people.

--- app/scripts/controllers/decryptWalletCtrl.js
+++ app/scripts/controllers/decryptWalletCtrl.js
@@ -24,7 +24,8 @@ var decryptWalletCtrl = function($scope, $sce, walletService) {
         trezorClassicPath: "m/44'/61'/0'/0",       // first address: m/44'/61'/0'/0/0
         trezorPath:        "m/44'/60'/0'/0",       // first address: m/44'/60'/0'/0/0
         hwUbqPath:         "m/44'/108'/0'/0",      // first address: m/44'/40'/0'/0/0
-        hwExpansePath:     "m/44'/40'/0'/0"        // first address: m/44'/40'/0'/0/0
+        hwExpansePath:     "m/44'/40'/0'/0",       // first address: m/44'/40'/0'/0/0
+        singularDTVPath:   "m/0'/0'/0'"            // first address: m/0'/0'/0'/0
     };
     $scope.HDWallet.dPath = $scope.HDWallet.defaultDPath;
     $scope.mnemonicModel = new Modal(document.getElementById('mnemonicModel'));
