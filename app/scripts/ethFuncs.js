818867bf1519e7a2fc22c62892a079cb510a6179
Chase Wright <mysticryuujin@gmail.com>
2017-11-13 11:12:58 -0600

    Update ethFuncs.js

--- app/scripts/ethFuncs.js
+++ app/scripts/ethFuncs.js
@@ -1,6 +1,6 @@
 'use strict';
 var ethFuncs = function() {}
-ethFuncs.gasAdjustment = 21;
+ethFuncs.gasAdjustment = 10;
 ethFuncs.validateEtherAddress = function(address) {
     if (address.substring(0, 2) != "0x") return false;
     else if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) return false;
