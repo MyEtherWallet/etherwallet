06198f2001494a796e41556bf04c2d6063327368
Anonoz Chong <honsiongchs@gmail.com>
2017-12-07 13:56:03 +0800

    Increased gas price slider upper limit due to CryptoKitties congestion.
    - Made the network full message appear at all times with link to ethgasstation (take it out after network clears up?)

--- app/scripts/controllers/tabsCtrl.js
+++ app/scripts/controllers/tabsCtrl.js
@@ -40,11 +40,11 @@ var tabsCtrl = function($scope, globalService, $translate, $sce) {
     }
     var setGasValues = function() {
         $scope.gas = {
-            curVal: 10,
-            value: globalFuncs.localStorage.getItem(gasPriceKey, null) ? parseInt(globalFuncs.localStorage.getItem(gasPriceKey)) : 10,
-            max: 60,
-            min: 0.1,
-            step: 0.1
+            curVal: 60,
+            value: globalFuncs.localStorage.getItem(gasPriceKey, null) ? parseInt(globalFuncs.localStorage.getItem(gasPriceKey)) : 60,
+            max: 120,
+            min: 1,
+            step: 1
         }
         ethFuncs.gasAdjustment = $scope.gas.value;
         $scope.gasPriceMsg = ethFuncs.gasAdjustment < 5 ? true : false
