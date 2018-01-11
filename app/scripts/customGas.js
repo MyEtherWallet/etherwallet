ed9e605124d14aced171eafd2621d1a9ecc4fed0
tayvano <tayvano@gmail.com>
2018-01-11 13:33:39 -0800

    Release: v3.11.2.3

--- app/scripts/customGas.js
+++ app/scripts/customGas.js
@@ -17,7 +17,7 @@ msg:        ' Accord (ARD) ERC20 token sale - http://accordtoken.com'
 to:         '0x16B0E62aC13a2fAeD36D18bce2356d25Ab3CfAD3',
 gasLimit:   200000,
 data:       '',
-msg:        'BTQ ICO ends February 1, 2018. btc btq is your exclusive bitcoin boutique and world's premier cryptocurrency lifestyle brand. https://thebtcbtq.com/btq'
+msg:        'BTQ ICO ends February 1, 2018. btc btq is your exclusive bitcoin boutique and world\'s premier cryptocurrency lifestyle brand. https://thebtcbtq.com/btq'
 },{
 // MNT Token Sale
 to:         '0xA9877b1e05D035899131DBd1e403825166D09f92',
