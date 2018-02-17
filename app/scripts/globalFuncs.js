8f85487a58ce928131745b0c544270b0be3e83ae
Kosala Hemachandra <kvhnuke@aol.com>
2018-02-16 16:26:31 -0800

    changed donation address

--- app/scripts/globalFuncs.js
+++ app/scripts/globalFuncs.js
@@ -154,7 +154,7 @@ globalFuncs.postDelay = 300;
 globalFuncs.kdf = "scrypt";
 globalFuncs.defaultTxGasLimit = 21000;
 globalFuncs.defaultTokenGasLimit = 200000;
-globalFuncs.donateAddress = "0x7cB57B5A97eAbe94205C07890BE4c1aD31E486A8";
+globalFuncs.donateAddress = "0xDECAF9CD2367cdbb726E904cD6397eDFcAe6068D";
 globalFuncs.isNumeric = function(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
