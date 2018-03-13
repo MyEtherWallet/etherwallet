1db7079fc8833120dbada5067ad717e39238c425
gamalielhere <gamalielgad1234@gmail.com>
2018-03-13 14:09:01 -0700

    Clear messages on state changes

--- app/scripts/directives/addressFieldDrtv.js
+++ app/scripts/directives/addressFieldDrtv.js
@@ -19,6 +19,9 @@ var addressFieldDrtv = function($compile, darkList) {
             }
 
             var checkDarkList = function(value) {
+              if(value.length === 0) {
+                console.log("Hello there general Kenobi!");
+              }
               for(let i = 0; i < Darklist.length; i++) {
                 if(value.length > 0 && value === Darklist[i].address) {
                   scope.phishing.msg = Darklist[i].comment !== '' ? `This address has been flagged: ${Darklist[i].comment}` : 'This address has been flagged in our Phishing list. Please make sure you are sending to the right address';
@@ -89,6 +92,8 @@ var addressFieldDrtv = function($compile, darkList) {
               } else {
                 setValue('');
                 scope.addressDrtv.showDerivedAddress = false;
+                scope.phishing.msg = '';
+                scope.phishing.error = false;
               }
 
             });
