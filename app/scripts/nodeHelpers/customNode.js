60bc0295cf6296341bd805c5993f18da0d79b32c
Kosala Hemachandra <kvhnuke@aol.com>
2018-08-17 19:37:54 -0700

    change eth_call to 'latest' as nodes require pruning disabled for 'pending'

--- app/scripts/nodeHelpers/customNode.js
+++ app/scripts/nodeHelpers/customNode.js
@@ -106,7 +106,7 @@ customNode.prototype.getEthCall = function(txobj, callback) {
             });
         }, 500);
     }
-    ethCallArr.calls.push({ "id": parentObj.getRandomID(), "jsonrpc": "2.0", "method": "eth_call", "params": [{ to: txobj.to, data: txobj.data }, 'pending'] });
+    ethCallArr.calls.push({ "id": parentObj.getRandomID(), "jsonrpc": "2.0", "method": "eth_call", "params": [{ to: txobj.to, data: txobj.data }, 'latest'] });
     ethCallArr.callbacks.push(callback);
 }
 customNode.prototype.getTraceCall = function(txobj, callback) {
