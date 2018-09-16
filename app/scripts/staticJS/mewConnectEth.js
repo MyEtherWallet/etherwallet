d9bcfc40996138713bbd266a0a451e2b0683fe2e
SteveM <SteveM@myEtherWallet.com>
2018-09-16 02:32:12 -0700

    add file missing from prior commit

---
+++ app/scripts/staticJS/mewConnectEth.js
@@ -0,0 +1,127 @@
+
+
+class MewConnectEth{
+  constructor (callback) {
+
+    this.listeners = [];
+
+    if (callback) {
+      this.callback = callback;
+    } else {
+      // this.callback = this.mewConnectCallback;
+    }
+    this.walletCallback = null;
+    this.signalerUrl =  "https://connect.mewapi.io" // nanobox hosted version
+  }
+
+  getCallback() {
+    return this.callback;
+  }
+
+  setMewConnect(mewConnect) {
+    this.comm = mewConnect;
+  }
+
+  signalerConnect(url) {
+    if (!url) {
+      this.comm.initiatorStart(this.signalerUrl);
+    } else {
+      this.comm.initiatorStart(url);
+    }
+  }
+
+  setWalletCallback(func) {
+    this.walletCallback = func;
+  }
+
+  createWallet(data) {
+    var address = data.address;
+    // var pub = data.pub;
+    var wallet = void 0;
+    if (address.substring(0, 2) != "0x") {
+      address = "0x" + address;
+    }
+    if (Validator.isValidAddress(address)) {
+      var tempWallet = new Wallet();
+      tempWallet.getAddressString = function () {
+        return address;
+      };
+      var balance = tempWallet.setBalance(function (data) {
+        return data;
+      });
+
+      wallet = {
+        // type: "addressOnly",
+        type: "mewConnect",
+        address: address,
+        // pubKey: pub,
+        getAddressString: tempWallet.getAddressString,
+        getChecksumAddressString: function getChecksumAddressString() {
+          return ethUtil.toChecksumAddress(this.getAddressString());
+        },
+        getBalance: function getBalance() {
+          return this.balance;
+        },
+        setBalance: tempWallet.setBalance,
+        setTokens: tempWallet.setTokens,
+        getPath: function getPath(stuff) {
+        },
+        getHWTransport: function getHWTransport(stuff) {
+        },
+        getHWType: function getHWType() {
+          return "mewConnect";
+        }
+      };
+      return wallet;
+    } else {
+      // todo add error message (address not valid)
+      return false;
+    }
+  }
+
+  signMessageSend(msg) {
+    var hashToSign = ethUtil.hashPersonalMessage(msg)
+    this.comm.sendRtcMessage("signMessage", hashToSign);
+  }
+
+  signTransaction(eTx, rawTx, txData) {
+    const sendTxData = {
+      nonce: rawTx.nonce,
+      gasPrice: rawTx.gasPrice,
+      to: rawTx.to,
+      value: rawTx.value,
+      data: rawTx.data,
+      chainId: rawTx.chainId,
+      gas: rawTx.gasLimit
+    }
+    this.comm.sendRtcMessage("signTx", JSON.stringify(sendTxData));
+  }
+
+  signMessage(messageHex) {
+    var self = this;
+    var hashToSign = ethUtil.hashPersonalMessage(ethUtil.toBuffer(messageHex))
+    self.comm.sendRtcMessage("signMessage", hashToSign.toString('hex'));
+  }
+
+  static getBrowserRTC() {
+    if (typeof window === 'undefined') return null;
+    var wrtc = {
+      RTCPeerConnection: window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection,
+      RTCSessionDescription: window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription,
+      RTCIceCandidate: window.RTCIceCandidate || window.mozRTCIceCandidate || window.webkitRTCIceCandidate
+    };
+    if (!wrtc.RTCPeerConnection) return null;
+    return wrtc;
+  }
+
+  static checkWebRTCAvailable() {
+
+    var doesNotHaveWebRTC = MewConnectEth.getBrowserRTC() == null;
+    return !doesNotHaveWebRTC;
+    // return false
+  }
+
+
+}
+
+module.exports = MewConnectEth
