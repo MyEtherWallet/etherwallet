09d3c1efc2d356160662e170530cecacd5432f25
Marko Bencun <mb@shiftdevices.com>
2018-11-28 17:26:13 +0100

    BitBox: compatibility with firmware 5.0.0

--- app/scripts/staticJS/digitalBitboxEth.js
+++ app/scripts/staticJS/digitalBitboxEth.js
@@ -22,31 +22,46 @@ var DigitalBitboxEth = function(comm, sec) {
     DigitalBitboxEth.to = setTimeout(function(){ DigitalBitboxEth.sec = ''; }, 60000);
 }
 
+var BitBoxSupportedMajorVersion = 5;
+
 DigitalBitboxEth.sec = '';
 DigitalBitboxEth.to = null;
 
 DigitalBitboxEth.aes_cbc_b64_decrypt = function(ciphertext, key) {
-    var res;
     try {
+        var h = Crypto.createHash('sha512').update(key).digest();
+        var encryptionKey = h.slice(0, 32);
+        var authenticationKey  = h.slice(32, 64);
+
         var ub64 = new Buffer(ciphertext, "base64").toString("binary");
-        var iv   = new Buffer(ub64.slice(0, 16), "binary");
-        var enc  = new Buffer(ub64.slice(16), "binary");
-        var decipher = Crypto.createDecipheriv("aes-256-cbc", key, iv);
+        var cipher = new Buffer(ub64.slice(0, ub64.length - 32), "binary");
+        var hmac = new Buffer(ub64.slice(ub64.length - 32), "binary");
+        var expectedHmac = Crypto.createHmac('sha256', authenticationKey).update(cipher).digest();
+        if (!hmac.equals(expectedHmac)) {
+            throw "hmac check failed";
+        }
+        var iv   = new Buffer(cipher.slice(0, 16), "binary");
+        var enc  = new Buffer(cipher.slice(16), "binary");
+        var decipher = Crypto.createDecipheriv("aes-256-cbc", encryptionKey, iv);
         var dec = decipher.update(enc) + decipher.final();
-        res = dec.toString("utf8");
+        return dec.toString("utf8");
     }
     catch(err) {
-        res = ciphertext;
+        return ciphertext;
     }
-    return res;
 }
 
 DigitalBitboxEth.aes_cbc_b64_encrypt = function(plaintext, key) {
     try {
+        var h = Crypto.createHash('sha512').update(key).digest();
+        var encryptionKey = h.slice(0, 32);
+        var authenticationKey  = h.slice(32, 64);
+
         var iv = Crypto.pseudoRandomBytes(16);
-        var cipher = Crypto.createCipheriv("aes-256-cbc", key, iv);
+        var cipher = Crypto.createCipheriv("aes-256-cbc", encryptionKey, iv);
         var ciphertext = Buffer.concat([iv, cipher.update(plaintext), cipher.final()]);
-        return ciphertext.toString("base64");
+        var hmac = Crypto.createHmac('sha256', authenticationKey).update(ciphertext).digest();
+        return Buffer.concat([ciphertext, hmac]).toString("base64");
     }
     catch(err) {
         return '';
@@ -65,15 +80,38 @@ DigitalBitboxEth.parseError = function(errObject) {
     return msg;
 }
 
-DigitalBitboxEth.prototype.getAddress = function(path, callback) {
+DigitalBitboxEth.prototype.send = function(cmd, callback) {
     var self = this;
-    var cmd = '{"xpub":"' + path + '"}';
-        cmd = DigitalBitboxEth.aes_cbc_b64_encrypt(cmd, this.key);
-    var localCallback = function(response, error) {
-		if (typeof error != "undefined") {
-			callback(undefined, error);
-		}
-		else {
+
+    this.comm.exchange('{"ping":""}', function(pingResponse, pingError) {
+        if (typeof pingError !== 'undefined') {
+			callback(undefined, pingError);
+            return;
+        }
+        pingResponse = JSON.parse(pingResponse.toString('utf8'));
+        if (!('device' in pingResponse)) {
+            callback(undefined, 'Please upgrade to the newest firmware using the <a href="https://shiftcrypto.ch/start" target="_blank">BitBox Desktop app.</a>');
+            return;
+        }
+        var match = (/^v(\d+)\.\d+\.\d+/).exec(pingResponse.device.version);
+        if (match === null || match.length != 2) {
+            throw 'unexpected reply';
+        }
+        var majorVersion = parseInt(match[1]);
+        if (majorVersion < BitBoxSupportedMajorVersion) {
+            callback(undefined, 'Please upgrade to the newest firmware using the <a href="https://shiftcrypto.ch/start" target="_blank">BitBox Desktop app.</a>');
+            return;
+        }
+        if (majorVersion > BitBoxSupportedMajorVersion) {
+            callback(undefined, 'MyEtherWallet does not yet support this version of the firmware');
+            return;
+        }
+        var cipher = DigitalBitboxEth.aes_cbc_b64_encrypt(cmd, self.key)
+        self.comm.exchange(cipher, function(response, error) {
+            if (typeof pingError !== 'undefined') {
+			    callback(undefined, pingError);
+                return;
+            }
             try {
                 response = JSON.parse(response.toString('utf8'));
                 if ('error' in response) {
@@ -86,71 +124,62 @@ DigitalBitboxEth.prototype.getAddress = function(path, callback) {
 				        callback(undefined, DigitalBitboxEth.parseError(response.error));
                         return;
                     }
-                    var hdkey = HDKey.fromExtendedKey(response.xpub);
-                    var result = {
-			            publicKey: hdkey.publicKey.toString('hex'),
-                        chainCode: hdkey.chainCode.toString('hex'),
-                    };
-                    callback(result);
-                    return;
+                    callback(response, undefined);
                 }
             }
             catch(err) {
                 callback(undefined, 'Unexpected error: ' + err.message);
             }
+        });
+    });
+}
+
+DigitalBitboxEth.prototype.getAddress = function(path, callback) {
+    var self = this;
+    var cmd = '{"xpub":"' + path + '"}';
+    var localCallback = function(response, error) {
+		if (typeof error != "undefined") {
+			callback(undefined, error);
+            return;
 		}
+		var hdkey = HDKey.fromExtendedKey(response.xpub);
+        var result = {
+			publicKey: hdkey.publicKey.toString('hex'),
+            chainCode: hdkey.chainCode.toString('hex'),
+        };
+        callback(result);
+        return;
 	};
-    self.comm.exchange(cmd, localCallback);
+    self.send(cmd, localCallback);
 }
 
 DigitalBitboxEth.signGeneric = function(self, path, chainId, hashToSign, callback) {
     var cmd = '{"sign":{"data":[{"hash":"' + hashToSign + '","keypath":"' + path + '"}]}}';
-        cmd = DigitalBitboxEth.aes_cbc_b64_encrypt(cmd, self.key);
-
     var localCallback = function(response, error) {
 		if (typeof error != "undefined") {
 			callback(undefined, error);
+            return;
 		}
-		else {
-            try {
-                response = JSON.parse(response.toString('utf8'));
-                if ('error' in response) {
-				    callback(undefined, DigitalBitboxEth.parseError(response.error));
-                    return;
-                }
-                if ('ciphertext' in response) {
-                    response = JSON.parse(DigitalBitboxEth.aes_cbc_b64_decrypt(response.ciphertext, self.key));
-                    if ('error' in response) {
-				        callback(undefined, DigitalBitboxEth.parseError(response.error));
-                        return;
-                    }
-                    if ('echo' in response) {
-                        // Echo from first sign command. (Smart verification not implemented.)
-                        // Send second sign command.
-                        var cmd = '{"sign":""}';
-                            cmd = DigitalBitboxEth.aes_cbc_b64_encrypt(cmd, self.key);
-                        self.comm.exchange(cmd, localCallback);
-                        return;
-                    }
-                    if ('sign' in response) {
-                        var vOffset = chainId ? chainId * 2 + 8 : 0;
-                        var v = new Buffer([parseInt(response.sign[0].recid, 16) + 27 + vOffset]);
-                        var result = {
-                            v: v.toString('hex'),
-                            r: response.sign[0].sig.slice(0, 64),
-                            s: response.sign[0].sig.slice(64, 128),
-                        };
-                        callback(result);
-                        return;
-                    }
-                }
-            }
-            catch(err) {
-                callback(undefined, 'Unexpected error:' + err.message);
-            }
-		}
+        if ('echo' in response) {
+            // Echo from first sign command. (Smart verification not implemented.)
+            // Send second sign command.
+            var cmd = '{"sign":""}';
+            self.send(cmd, localCallback);
+            return;
+        }
+        if ('sign' in response) {
+            var vOffset = chainId ? chainId * 2 + 8 : 0;
+            var v = new Buffer([parseInt(response.sign[0].recid, 16) + 27 + vOffset]);
+            var result = {
+                v: v.toString('hex'),
+                r: response.sign[0].sig.slice(0, 64),
+                s: response.sign[0].sig.slice(64, 128),
+            };
+            callback(result);
+            return;
+        }
 	};
-    self.comm.exchange(cmd, localCallback);
+    self.send(cmd, localCallback);
 }
 
 DigitalBitboxEth.prototype.signTransaction = function(path, eTx, callback) {
