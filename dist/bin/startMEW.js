8ee713e27c926304fb1aaf7d61ab625652a14e88
SteveM <SteveM@myEtherWallet.com>
2019-06-10 17:44:39 -0700

    show token info on ledger

--- dist/bin/startMEW.js
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env node
-
-var open = require("open");
-var separator = process.platform=="win32" ? "\\" : "/";
-open(require('path').dirname(require.main.filename)+separator+".."+separator+"index.html");
